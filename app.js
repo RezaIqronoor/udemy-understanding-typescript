fac1da7f4efdd1697115087569ee08d8ba83ca10
rezaiqronoor <rezaiqronoor.dev@gmail.com>
2022-03-07 21:24:31 +0700

    commit

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-/**
- * This is an Unkown Type, it's the same as
- * Any Type where you can store any value to the variable except
- * you can't freely assign it to another defined variable.
- *
- * Like the example below you need to type check it or else it will
- * yields an error unlike Any Type.
- */
-var userInput;
-var userName;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("An error occured", 500);
+"use strict";
+const userName = "Regayaku";
