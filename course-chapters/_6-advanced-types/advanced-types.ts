0b79b08469a9510fe32012bc2c02744db858bea2
rezaiqronoor <rezaiqronoor.dev@gmail.com>
2022-03-12 13:37:26 +0700

    commit

--- course-chapters/_6-advanced-types/advanced-types.ts
+++ course-chapters/_6-advanced-types/advanced-types.ts
@@ -1,18 +1,54 @@
-"use strict";
-var _a;
-const e1 = {
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+/**
+ * Intersection Type, intersect / combine 2 types
+ * if its an Object, it combine both object.
+ * if its a Union Type it take the common type that exist in both union type.
+ */
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
     name: "Max",
     privileges: ["create-server"],
     startDate: new Date(),
 };
-function add(a, b) {
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+/**
+ * Function Overloads lets you decide the return value of a function
+ * when a specific parameters is inserted.
+ * Problem: result.split() cannot be called because result value is of
+ * Combinable, with Function Overload this can be fixed by explicitly saying
+ * that return type will be string so the result value will be string.
+ */
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
     if (typeof a === "string" || typeof b === "string") {
         return a.toString() + b.toString();
     }
     return a + b;
 }
+
 const result = add("Max", " Bob");
 result.split(" ");
+
 const fetchedUserData = {
     id: "ul",
     name: "Max",
@@ -24,17 +60,22 @@ const fetchedUserData = {
  * Ex: if .job or .job.title not exist, the code would just yield undefined
  * rather than throw a runtime error.
  */
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+console.log(fetchedUserData?.job?.title);
+
 const userInput = "";
+
 /**
  * Nullish Coalescing, it tells you wether a variable is really
  * just null or undefined, not empty string or anything else.
  * Here if userInput is really just null or undefined then it will
  * yield true and "DEFAULT" will be called.
  */
-const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
+const storedData = userInput ?? "DEFAULT";
+
 console.log(storedData);
+
 // type UnknownEmployee = Employee | Admin;
+
 // function printEmployeeInformation(emp: UnknownEmployee) {
 //     console.log("Name: " + emp.name);
 //     /**
@@ -48,23 +89,30 @@ console.log(storedData);
 //         console.log("Start Date: " + emp.startDate);
 //     }
 // }
+
 // printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
 // class Car {
 //     drive() {
 //         console.log("Driving...");
 //     }
 // }
+
 // class Truck {
 //     drive() {
 //         console.log("Driving a Truck...");
 //     }
+
 //     loadCargo(amount: number) {
 //         console.log("Loading cargo... " + amount);
 //     }
 // }
+
 // type Vehicle = Car | Truck;
+
 // const v1 = new Car();
 // const v2 = new Truck();
+
 // function useVehicle(vehicle: Vehicle) {
 //     vehicle.drive();
 //     /**
@@ -74,8 +122,10 @@ console.log(storedData);
 //         vehicle.loadCargo(1000);
 //     }
 // }
+
 // useVehicle(v1);
 // useVehicle(v2);
+
 // /**
 //  * "type: 'bird'" here is called Dicrimated Unions, which goes the same way
 //  * for horse.
@@ -87,11 +137,14 @@ console.log(storedData);
 //     type: "bird";
 //     flyingSpeed: number;
 // }
+
 // interface Horse {
 //     type: "horse";
 //     runningSpeed: number;
 // }
+
 // type Animal = Bird | Horse;
+
 // function moveAnimal(animal: Animal) {
 //     let speed;
 //     switch (animal.type) {
@@ -104,7 +157,9 @@ console.log(storedData);
 //     }
 //     console.log("Moving with speed: " + speed);
 // }
+
 // moveAnimal({ type: "bird", flyingSpeed: 200 });
+
 // /**
 //  * This is Type Casting, you can assign a specific type if Typescript cannot
 //  * determine it on its own
@@ -114,9 +169,11 @@ console.log(storedData);
 // // const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
 // // const userInputElement = document.getElementById("user-input")! as HTMLInputElement;
 // const userInputElement = document.getElementById("user-input");
+
 // if (document.getElementById("user-input")) {
 //     (userInputElement as HTMLInputElement).value = "Hi There!";
 // }
+
 // /**
 //  * Index Properties, TLDR it tells you that you with this interface you can add any amount
 //  * of object properties but the property name type must be or can be converted to string (ie: number). And the
@@ -127,8 +184,8 @@ console.log(storedData);
 // interface ErrorContainer { // { email: "Not a valid email", username: "Must start with a capital character"}
 //     [prop: string]: string;
 // }
+
 // const errorBag: ErrorContainer = {
 //     email: "Not a valid email",
 //     username: "Must start with a capital character!"
 // };
-//# sourceMappingURL=app.js.map
