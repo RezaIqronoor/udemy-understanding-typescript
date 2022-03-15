ecbde6edd967c11ffaf4c4fa1201db2470dbb1da
rezaiqronoor <rezaiqronoor.dev@gmail.com>
2022-03-15 21:31:13 +0700

    commit

--- dist/app.js
+++ dist/app.js
@@ -5,138 +5,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-/**
- * Declaring Decoraters, it takes the class's constructor function as a parameter
- * The declaration of decorateor run first but the constructor function runs bottom up start
- *
- * This is a Decorator Factory function, you will be able to accept custom arguments
- * with this.
- */
-function Logger(logString) {
-    console.log("LOGGER FACTORY");
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-}
-/**
- * More useful decorators.
- * This function takes template ( html string ) and hookId ( html element id )
- * to modify an existing html element by adding <h1> then changing
- * the <h1> content to name which is a property exist in class Person.
- * this happen in the first return function which will fired at when the class is created.
- *
- * Take an extra step by returning a class / constructor inside of the first return statement that will
- * keep the former class property but you can add another custom logic inside of it.
- * The difference here is the logic inside the new class / constructor will not be
- * fired when the class is created but when the class is instantiated.
- */
-function WithTemplate(template, hookId) {
-    console.log("TEMPLATE FACTORY");
-    return function (originalConstructor) {
-        return class extends originalConstructor {
-            constructor(..._) {
-                super();
-                console.log("Rendering Template");
-                const hookEl = document.getElementById(hookId);
-                if (hookEl) {
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector("h1").textContent = this.name;
-                }
-            }
-        };
-    };
-}
-// @Logger("LOGGING - PERSON")
-let Person = class Person {
-    constructor() {
-        this.name = "Max";
-        console.log("Creating person object...");
-    }
-};
-Person = __decorate([
-    Logger("Logging"),
-    WithTemplate("<h1>My Person Object<h1>", "app")
-], Person);
-const pers = new Person();
-console.log(pers);
-/**
- * This is a Property Decorator, to access informations that the property has.
- * target if an instance will return prototype but if static will return constructor.
- */
-function Log(target, propertyName) {
-    console.log("Property Decorator!");
-    console.log(target, propertyName);
-}
-/**
- * Accessor Decorator
- */
-function Log2(target, name, descriptor) {
-    console.log("Accessor Decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-/**
- * Method Decorator
- */
-function Log3(target, name, descriptor) {
-    console.log("Method Decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-/**
- * Parameter Decorator
- */
-function Log4(target, name, position) {
-    console.log("Parameter Decorator");
-    console.log(target);
-    console.log(name);
-    console.log(position);
-}
-class Product {
-    constructor(t, p) {
-        this.title = t;
-        this._price = p;
-    }
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error("Invalid Price, should be positive.");
-        }
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
-    }
-}
-__decorate([
-    Log
-], Product.prototype, "title", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "getPriceWithTax", null);
-const p1 = new Product("Book", 19);
-const p2 = new Product("Book2", 10);
-/**
- * Since this refers to the thing that responsible to call the method,
- * if addeventlistener calls it it will return undefined.
- * Below here is an Auto Bind descriptor example.
- */
-function AutoBiind(_, _2, descriptor) {
+// Autobind Decorator
+function autobind(target, methodName, descriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor = {
         configurable: true,
-        enumerable: false,
         get() {
             const boundFn = originalMethod.bind(this);
             return boundFn;
@@ -144,72 +17,57 @@ function AutoBiind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
-class Printer {
+// ProjectInput Class
+class ProjectInput {
     constructor() {
-        this.message = "This Works!";
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
+        this.attach();
     }
-    showMessage() {
-        console.log(this.message);
-    }
-}
-__decorate([
-    AutoBiind
-], Printer.prototype, "showMessage", null);
-const p = new Printer();
-const button = document.querySelector("button");
-button.addEventListener("click", p.showMessage);
-const registeredValidators = {};
-function Required(target, propName) {
-    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ["required"] });
-}
-function PositiveNumber(target, propName) {
-    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ["positive"] });
-}
-function validate(obj) {
-    const objValidatorConfig = registeredValidators[obj.constructor.name];
-    if (!objValidatorConfig) {
-        return true;
-    }
-    let isValid = true;
-    console.log(objValidatorConfig);
-    for (const prop in objValidatorConfig) {
-        for (const validator of objValidatorConfig[prop]) {
-            switch (validator) {
-                case "required":
-                    isValid = isValid && !!obj[prop];
-                    break;
-                case "positive":
-                    isValid = isValid && obj[prop] > 0;
-                    break;
-            }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0) {
+            alert("Invalid Input, please try again!");
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
         }
     }
-    return isValid;
-}
-class Course {
-    constructor(t, p) {
-        this.title = t;
-        this.price = p;
+    clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
 __decorate([
-    Required
-], Course.prototype, "title", void 0);
-__decorate([
-    PositiveNumber
-], Course.prototype, "price", void 0);
-const courseForm = document.querySelector("form");
-courseForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const titleElement = document.getElementById("title");
-    const priceElement = document.getElementById("price");
-    const title = titleElement.value;
-    const price = +priceElement.value;
-    const createdCourse = new Course(title, price);
-    if (!validate(createdCourse)) {
-        alert("Invalid Input, please try again!");
-        return;
-    }
-    console.log(createdCourse);
-});
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
