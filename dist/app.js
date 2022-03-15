84e1106a58b21e12d3c5acf9937d0a2dbe840dd1
rezaiqronoor <rezaiqronoor.dev@gmail.com>
2022-03-15 10:40:17 +0700

    commit

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 /**
  * Declaring Decoraters, it takes the class's constructor function as a parameter
+ * The declaration of decorateor run first but the constructor function runs bottom up start
  *
  * This is a Decorator Factory function, you will be able to accept custom arguments
  * with this.
  */
 function Logger(logString) {
+    console.log("LOGGER FACTORY");
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
@@ -22,15 +27,27 @@ function Logger(logString) {
  * This function takes template ( html string ) and hookId ( html element id )
  * to modify an existing html element by adding <h1> then changing
  * the <h1> content to name which is a property exist in class Person.
+ * this happen in the first return function which will fired at when the class is created.
+ *
+ * Take an extra step by returning a class / constructor inside of the first return statement that will
+ * keep the former class property but you can add another custom logic inside of it.
+ * The difference here is the logic inside the new class / constructor will not be
+ * fired when the class is created but when the class is instantiated.
  */
 function WithTemplate(template, hookId) {
-    return function (constructor) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1").textContent = p.name;
-        }
+    console.log("TEMPLATE FACTORY");
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log("Rendering Template");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = this.name;
+                }
+            }
+        };
     };
 }
 // @Logger("LOGGING - PERSON")
@@ -41,8 +58,73 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    WithTemplate('<h1>My Person Object<h1>', "app")
+    Logger("Logging"),
+    WithTemplate("<h1>My Person Object<h1>", "app")
 ], Person);
 const pers = new Person();
 console.log(pers);
+/**
+ * This is a Property Decorator, to access informations that the property has.
+ * target if an instance will return prototype but if static will return constructor.
+ */
+function Log(target, propertyName) {
+    console.log("Property Decorator!");
+    console.log(target, propertyName);
+}
+/**
+ * Accessor Decorator
+ */
+function Log2(target, name, descriptor) {
+    console.log("Accessor Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+/**
+ * Method Decorator
+ */
+function Log3(target, name, descriptor) {
+    console.log("Method Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+/**
+ * Parameter Decorator
+ */
+function Log4(target, name, position) {
+    console.log("Parameter Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid Price, should be positive.");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book2", 10);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 /**
  * Declaring Decoraters, it takes the class's constructor function as a parameter
- * 
+ * The declaration of decorateor run first but the constructor function runs bottom up start
+ *
  * This is a Decorator Factory function, you will be able to accept custom arguments
  * with this.
  */
 function Logger(logString: string) {
+    console.log("LOGGER FACTORY");
     return function (constructor: Function) {
         console.log(logString);
         console.log(constructor);
@@ -12,24 +14,39 @@ function Logger(logString: string) {
 }
 
 /**
- * More useful decorators. 
+ * More useful decorators.
  * This function takes template ( html string ) and hookId ( html element id )
  * to modify an existing html element by adding <h1> then changing
  * the <h1> content to name which is a property exist in class Person.
+ * this happen in the first return function which will fired at when the class is created.
+ *
+ * Take an extra step by returning a class / constructor inside of the first return statement that will
+ * keep the former class property but you can add another custom logic inside of it. 
+ * The difference here is the logic inside the new class / constructor will not be
+ * fired when the class is created but when the class is instantiated.
  */
 function WithTemplate(template: string, hookId: string) {
-    return function(constructor: any) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if(hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1")!.textContent = p.name;
-        }
-    }
+    console.log("TEMPLATE FACTORY");
+    return function <T extends { new (...args: any[]): { name: string } }>(
+        originalConstructor: T
+    ) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log("Rendering Template");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1")!.textContent = this.name;
+                }
+            }
+        };
+    };
 }
 
 // @Logger("LOGGING - PERSON")
-@WithTemplate('<h1>My Person Object<h1>', "app")
+@Logger("Logging")
+@WithTemplate("<h1>My Person Object<h1>", "app")
 class Person {
     name = "Max";
 
@@ -41,3 +58,74 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+/**
+ * This is a Property Decorator, to access informations that the property has.
+ * target if an instance will return prototype but if static will return constructor.
+ */
+function Log(target: any, propertyName: string | Symbol) {
+    console.log("Property Decorator!");
+    console.log(target, propertyName);
+}
+
+/**
+ * Accessor Decorator
+ */
+function Log2(
+    target: any,
+    name: string,
+    descriptor: PropertyDescriptor
+) {
+    console.log("Accessor Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+/**
+ * Method Decorator
+ */
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log("Method Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+/**
+ * Parameter Decorator
+ */
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log("Parameter Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error("Invalid Price, should be positive.");
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+}
+
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book2", 10);
