e95b3a4342b90480a96128168e8912045d4ad960
rezaiqronoor <rezaiqronoor.dev@gmail.com>
2022-03-21 21:57:10 +0700

    commit

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,8 @@
-import _ from "lodash";
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from "./product.model";
+const products = [
+    { title: "A Carpet", price: 29.99 },
+    { title: "A Book", price: 10.99 }
+];
+const p1 = new Product("A Book", 12.99);
+console.log(p1.getInformation());
 //# sourceMappingURL=app.js.map
